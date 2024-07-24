30e365edd22af6754424c870db093b563a90c833
Marcos Oliveira Gomes <115948731+marcosogg@users.noreply.github.com>
2024-07-24 22:18:29 +0100

    Add to favourites logic completed; Dummy favourites page.

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -40,6 +40,13 @@ const MovieListPage = (props) => {
     else setGenreFilter(value);
   };
   
+  const addToFavourites = (movieId) => {
+    const updatedMovies = movies.map((m) =>
+      m.id === movieId ? { ...m, favourite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
+
   useEffect(() => {
     fetch(
       `https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&page=1`
@@ -55,14 +62,14 @@ const MovieListPage = (props) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return (
+return (
     <>
       <Grid container sx={styles.root}>
         <Grid item xs={12}>
           <Header title={"Home Page"} />
         </Grid>
         <Grid item container spacing={5}>
-          <MovieList movies={displayedMovies}></MovieList>
+        <MovieList movies={displayedMovies} selectFavourite={addToFavourites} />
         </Grid>
       </Grid>
       <Fab
@@ -87,4 +94,5 @@ const MovieListPage = (props) => {
     </>
   );
 };
+
 export default MovieListPage;
